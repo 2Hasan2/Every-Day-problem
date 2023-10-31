272dcd2142776c04100b024d6961a6fd1b124e0a
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-31 15:23:53 +0200

    `001.js` in code camp

---
+++ CodeCamp/001.js
@@ -0,0 +1,9 @@
+function multiplesOf3and5(n) {
+    let i = 0, sum = 0;
+    while (n > i) {
+        sum += (i % 3 === 0) || (i % 5 === 0) ? i : 0;
+        i++;
+    }
+    return sum;
+}
+console.log(multiplesOf3and5(15));
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Every Day problem
- 
