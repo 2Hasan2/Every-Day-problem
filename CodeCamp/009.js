49edadb41570507ec113562d96419e5187fa83c6
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-31 21:57:46 +0200

    `009.js`

---
+++ CodeCamp/009.js
@@ -0,0 +1,38 @@
+class Triplet {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+        this.c = Math.sqrt(a * a + b * b);
+    }
+
+    sum() {
+        return this.a + this.b + this.c;
+    }
+
+    product() {
+        return this.a * this.b * this.c;
+    }
+
+    isPythagorean() {
+        return this.a ** 2 + this.b ** 2 === this.c ** 2;
+    }
+
+}
+
+let triplet = new Triplet(3, 4);
+
+function specialPythagoreanTriplet(n) {
+    let a = 1, b = 2;
+    while (a < n) {
+        while (b < n) {
+            let triplet = new Triplet(a, b);
+            if (triplet.sum() === n && triplet.isPythagorean()) {
+                return triplet.product();
+            }
+            b++;
+        }
+        a++;
+        b = a + 1;
+    }
+}
+console.log(specialPythagoreanTriplet(24));
