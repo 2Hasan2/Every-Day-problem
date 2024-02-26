9ebfaca80da355d1ccf3483b75fe36b25be00e6d
2Hasan2 <h01554176846@gmail.com>
2024-02-26 16:08:00 +0200

    2024-02-26 04:08:00 PM

--- hackerrank/Matrix Layer Rotation.js
+++ hackerrank/Matrix Layer Rotation.js
@@ -1,44 +1,51 @@
 function matrixRotation(matrix, r) {
-	let y = matrix.length;
-	let x = matrix[0].length;
-	let layers = Math.min(x, y) / 2;
+	const y = matrix.length;
+	const x = matrix[0].length;
+	const layers = Math.min(x, y) / 2;
 
 	for (let layer = 0; layer < layers; layer++) {
-		let layerLength = (x - layer * 2) * 2 + (y - layer * 2 - 2) * 2;
-		let rotation = r % layerLength;
+		const layerLength = (x - layer * 2) * 2 + (y - layer * 2 - 2) * 2;
+		const rotation = r % layerLength;
+
 		for (let rot = 0; rot < rotation; rot++) {
-			let temp = matrix[layer][layer];
-			// Move elements of top row.
+			// Store the top-left element of the current layer
+			const temp = matrix[layer][layer];
+
+			// Rotate top row
 			for (let i = layer; i < x - layer - 1; i++) {
 				matrix[layer][i] = matrix[layer][i + 1];
 			}
-			// Move elements of right column.
+
+			// Rotate right column
 			for (let i = layer; i < y - layer - 1; i++) {
 				matrix[i][x - layer - 1] = matrix[i + 1][x - layer - 1];
 			}
-			// Move elements of bottom row.
+
+			// Rotate bottom row
 			for (let i = x - layer - 1; i > layer; i--) {
 				matrix[y - layer - 1][i] = matrix[y - layer - 1][i - 1];
 			}
-			// Move elements of left column.
+
+			// Rotate left column
 			for (let i = y - layer - 1; i > layer + 1; i--) {
 				matrix[i][layer] = matrix[i - 1][layer];
 			}
+
+			// Assign the stored top-left element to its new position
 			matrix[layer + 1][layer] = temp;
 		}
 	}
-	return matrix;
+
+	for (let i = 0; i < matrix.length; i++) {
+		console.log(matrix[i].join(' '));
+	}
 }
+
 let matrix = [
 	[1, 2, 3],
 	[5, 6, 7],
 	[9, 1, 1],
 ];
 
-printMatrix(matrixRotation(matrix, 2));
 
-function printMatrix(matrix) {
-	for (let i = 0; i < matrix.length; i++) {
-		console.log(matrix[i].join(' '));
-	}
-}
+matrixRotation(matrix, 3)
