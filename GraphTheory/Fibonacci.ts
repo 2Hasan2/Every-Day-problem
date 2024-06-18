15bc71d1b0d1bbbc5765d6888e1c677796fa8b76
2Hasan2 <h01554176846@gmail.com>
2024-06-18 15:20:39 -0400

    2024-06-18 03:20:39 PM

--- GraphTheory/Fibonacci.ts
+++ GraphTheory/Fibonacci.ts
@@ -7,4 +7,19 @@ const fib = (n: number, memo: number[] = []): number => {
   memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
   return memo[n];
 }
-console.log(fib(99));
+
+//* using tabulation
+const fib2 = (n: number): number => {
+  const table = Array(n + 1).fill(0);
+  table[1] = 1;
+  for (let i = 0; i <= n; i++) {
+    table[i + 1] += table[i];
+    table[i + 2] += table[i];
+  }
+  return table[n];
+}
+
+console.log(fib(1_000_000));
+console.log(fib2(1_000_000));
+
+// console.log(fib(6));
--- GraphTheory/GridTraveler.ts
+++ GraphTheory/GridTraveler.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env ts-node
 //? 2. Grid Traveler
 //* using memoization
-
 function gridTraveler(row: number, col: number, memo: Record<string, number> = {}): number {
   const key = `${row},${col}`
   if (key in memo) { return memo[key] }
@@ -13,3 +12,19 @@ function gridTraveler(row: number, col: number, memo: Record<string, number> = {
 
 console.log(gridTraveler(6, 6));
 
+
+//* using tabulation
+function gridTraveler2(row: number, col: number): number {
+  const table = Array(row + 1).fill(0).map(() => Array(col + 1).fill(0));
+  table[1][1] = 1;
+  for (let i = 0; i <= row; i++) {
+    for (let j = 0; j <= col; j++) {
+      const current = table[i][j];
+      if (j + 1 <= col) table[i][j + 1] += current;
+      if (i + 1 <= row) table[i + 1][j] += current;
+    }
+  }
+  return table[row][col];
+}
+
+console.log(gridTraveler2(3, 3));
