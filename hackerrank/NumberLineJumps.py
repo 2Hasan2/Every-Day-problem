df38f7b8eded41552ebb6768f51a471f48d63e6d
2Hasan2 <h01554176846@gmail.com>
2024-08-08 16:41:25 +0300

    2024-08-08 04:41:25 م

---
+++ hackerrank/NumberLineJumps.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+def kangaroo(x1, v1, x2, v2):
+    if v1 == v2:
+        return "YES" if x1 == x2 else "NO"
+    if (x2 - x1) * (v1 - v2) >= 0 and (x2 - x1) % (v1 - v2) == 0:
+        return "YES"
+    else:
+        return "NO"
